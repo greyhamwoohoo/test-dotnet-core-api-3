5007bf8cb9f501bfe5ef72ef7b70861ac5edbddf
greyhamwoohoo <46085273+greyhamwoohoo@users.noreply.github.com>
2020-05-26 22:24:51 +1000

    Fixing up non-working environment variable assignments

--- azure-devops/wait-until-uri-is-ready.ps1
+++ azure-devops/wait-until-uri-is-ready.ps1
@@ -10,15 +10,18 @@ URI of the endpoint to ping
 
 .EXAMPLE
 . \wait-until-uri-is-ready.ps1 -TimeoutInSeconds 60 -Uri "http://localhost:80/whatever" -ExpectStatusCode 200
+
+.LINK
+https://github.com/greyhamwoohoo/netcore-selenium-framework/blob/master/azure-devops/wait-until-uri-is-ready.ps1
 #>
 Param(
     [Parameter(Mandatory,HelpMessage="Specify timeout in seconds")]
-    [int] $TimeoutInSeconds = [System.Environment]::ExpandEnvironmentVariables("WAIT_UNTIL_URI_IS_READY_TIMEOUT_IN_SECONDS"),
+    [int] $TimeoutInSeconds,
     [Parameter(Mandatory,HelpMessage="Uri of endpoint to query")]
     [ValidateNotNullOrEmpty()]
-    [string] $Uri = [System.Environment]::ExpandEnvironmentVariables("WAIT_UNTIL_URI_IS_READY_URI"),
+    [string] $Uri,
     [Parameter(Mandatory,HelpMessage="Specify successful StatusCode")]
-    [int] $ExpectStatusCode = [System.Environment]::ExpandEnvironmentVariables("WAIT_UNTIL_URI_IS_READY_EXPECT_STATUS_CODE")
+    [int] $ExpectStatusCode
 )
 PROCESS
 {
